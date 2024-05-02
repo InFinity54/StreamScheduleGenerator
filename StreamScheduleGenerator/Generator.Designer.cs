021404fa3e624a9da0c9d3c6b160c5d0420e9267
InFinity_54 <corentinkreicher@gmail.com>
2024-05-02 03:27:11 +0200

    Création de l'apparence + choix du mois du planning

--- StreamScheduleGenerator/Generator.Designer.cs
+++ StreamScheduleGenerator/Generator.Designer.cs
@@ -28,19 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Generator));
+            Header_Icon = new PictureBox();
+            Header_Title = new Label();
+            Header_Settings = new Button();
+            PlanningMonthSelection_Month = new ComboBox();
+            PlanningMonthSelection_Year = new ComboBox();
+            PlanningMonthSelection_Label = new Label();
+            ((System.ComponentModel.ISupportInitialize)Header_Icon).BeginInit();
             SuspendLayout();
+            // 
+            // Header_Icon
+            // 
+            Header_Icon.Image = Properties.Resources.app_icon;
+            Header_Icon.Location = new Point(12, 12);
+            Header_Icon.Name = "Header_Icon";
+            Header_Icon.Size = new Size(50, 50);
+            Header_Icon.SizeMode = PictureBoxSizeMode.Zoom;
+            Header_Icon.TabIndex = 0;
+            Header_Icon.TabStop = false;
+            // 
+            // Header_Title
+            // 
+            Header_Title.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Header_Title.Location = new Point(68, 12);
+            Header_Title.Name = "Header_Title";
+            Header_Title.Size = new Size(421, 50);
+            Header_Title.TabIndex = 1;
+            Header_Title.Text = "Générateur de planning de stream personnalisé";
+            Header_Title.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // Header_Settings
+            // 
+            Header_Settings.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            Header_Settings.Image = Properties.Resources.app_header_settingsbuttonicon;
+            Header_Settings.Location = new Point(1005, 12);
+            Header_Settings.Name = "Header_Settings";
+            Header_Settings.Size = new Size(50, 50);
+            Header_Settings.TabIndex = 2;
+            Header_Settings.UseVisualStyleBackColor = true;
+            // 
+            // PlanningMonthSelection_Month
+            // 
+            PlanningMonthSelection_Month.FormattingEnabled = true;
+            PlanningMonthSelection_Month.Items.AddRange(new object[] { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" });
+            PlanningMonthSelection_Month.Location = new Point(685, 26);
+            PlanningMonthSelection_Month.Name = "PlanningMonthSelection_Month";
+            PlanningMonthSelection_Month.Size = new Size(121, 25);
+            PlanningMonthSelection_Month.TabIndex = 3;
+            // 
+            // PlanningMonthSelection_Year
+            // 
+            PlanningMonthSelection_Year.FormattingEnabled = true;
+            PlanningMonthSelection_Year.Location = new Point(812, 26);
+            PlanningMonthSelection_Year.Name = "PlanningMonthSelection_Year";
+            PlanningMonthSelection_Year.Size = new Size(121, 25);
+            PlanningMonthSelection_Year.TabIndex = 4;
+            // 
+            // PlanningMonthSelection_Label
+            // 
+            PlanningMonthSelection_Label.Location = new Point(524, 26);
+            PlanningMonthSelection_Label.Name = "PlanningMonthSelection_Label";
+            PlanningMonthSelection_Label.Size = new Size(155, 25);
+            PlanningMonthSelection_Label.TabIndex = 5;
+            PlanningMonthSelection_Label.Text = "Planning du mois de :";
+            PlanningMonthSelection_Label.TextAlign = ContentAlignment.MiddleRight;
             // 
             // Generator
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = Color.FromArgb(33, 33, 33);
+            ClientSize = new Size(1067, 579);
+            Controls.Add(PlanningMonthSelection_Label);
+            Controls.Add(PlanningMonthSelection_Year);
+            Controls.Add(PlanningMonthSelection_Month);
+            Controls.Add(Header_Settings);
+            Controls.Add(Header_Title);
+            Controls.Add(Header_Icon);
+            Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = Color.White;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
             Name = "Generator";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Form1";
+            Text = "Générateur de planning de stream personnalisé";
+            ((System.ComponentModel.ISupportInitialize)Header_Icon).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox Header_Icon;
+        private Label Header_Title;
+        private Button Header_Settings;
+        private ComboBox PlanningMonthSelection_Month;
+        private ComboBox PlanningMonthSelection_Year;
+        private Label PlanningMonthSelection_Label;
     }
 }
--- StreamScheduleGenerator/Generator.cs
+++ StreamScheduleGenerator/Generator.cs
@@ -5,6 +5,18 @@ namespace StreamScheduleGenerator
         public Generator()
         {
             InitializeComponent();
+
+            // Sélection du mois courante
+            PlanningMonthSelection_Month.SelectedIndex = DateTime.Now.Month - 1;
+
+            // Initialisation de la liste des années
+            for (int i = DateTime.Now.Year;  i <= DateTime.Now.Year + 10; i++)
+            {
+                PlanningMonthSelection_Year.Items.Add(i.ToString());
+            }
+
+            // Sélection de l'année courante
+            PlanningMonthSelection_Year.SelectedIndex = 0;
         }
     }
 }
