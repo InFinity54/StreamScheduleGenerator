e58da34037b58f9e3be15be46ddd997a44b28120
InFinity_54 <corentinkreicher@gmail.com>
2024-07-25 08:31:44 +0200

    Suppression de la couleur contrastée (inutilisée) + persistance des paramètres

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,28 @@
-window.appSettings = {};
+import { initIdentityValues } from "./steps/identity";
+
+window.appSettings = {
+  identity: {
+    platform: 'twitch',
+    platformColor: 'default',
+    channelName: '',
+    fullChannelLink: false,
+    fontName: '',
+    colors: {
+      titles: '#FFFFFF',
+      dayOn: '#FFFFFF',
+      dayOff: '#FF0000'
+    }
+  }
+};
 
 window.appAPI.noSettingsFile((event) => {
   window.appAPI.saveSettings(JSON.stringify(window.appSettings));
+  initIdentityValues();
 });
 
 window.appAPI.updateSettings((event, args) => {
   window.appSettings = JSON.parse(args);
+  initIdentityValues();
 });
 
 window.updaterAPI.noUpdateAvailable((event) => {
--- src/js/steps/identity.js
+++ src/js/steps/identity.js
@@ -5,6 +5,22 @@ jQuery("#identity_button_previous").click(function () {
 
 jQuery("#identity_button_next").click(function () {
     if (jQuery('#identity_form')[0].checkValidity()) {
+        window.appSettings = {
+            identity: {
+                platform: 'twitch',
+                platformColor: jQuery("#identity_twitchlogocolor").val(),
+                channelName: jQuery("#identity_twitchusername").val(),
+                fullChannelLink: jQuery("#identity_twitchfulllink")[0].checked,
+                fontName: jQuery("#identity_font").val(),
+                colors: {
+                    titles: jQuery("#identity_titlescolor").val(),
+                    dayOn: jQuery("#identity_dayoncolor").val(),
+                    dayOff: jQuery("#identity_dayoffcolor").val()
+                }
+            }
+        };
+        window.appAPI.saveSettings(JSON.stringify(window.appSettings));
+
         jQuery("#identity_error").addClass("d-none");
         jQuery("#identity").fadeOut("500");
         jQuery("#planning").delay(500).fadeIn("500");
@@ -41,22 +57,29 @@ jQuery("#identity_button_next").click(function () {
             errorMessage += `La couleur des jours OFF n'a pas été renseignée.`;
         }
 
-        if (!jQuery("#identity_dayoffcontrastcolor")[0].validity.valid) {
-            if (errorMessage.length > 0) errorMessage += `<br />`;
-            errorMessage += `La couleur contrastée des jours OFF n'a pas été renseignée.`;
-        }
-
         jQuery("#identity_error").html(errorMessage);
     }
 });
 
-window.appAPI.fontsList((event, fonts) => {
-    for (var font in fonts) {
-        jQuery('#identity_font').append(
-            jQuery('<option>', {
-                value: fonts[font].replaceAll("\"", ""),
-                text: fonts[font].replaceAll("\"", "")
-            })
-        );
-    }
-});
+function initIdentityValues() {
+    window.appAPI.fontsList((event, fonts) => {
+        for (var font in fonts) {
+            jQuery('#identity_font').append(
+                jQuery('<option>', {
+                    value: fonts[font].replaceAll("\"", ""),
+                    text: fonts[font].replaceAll("\"", ""),
+                    selected: fonts[font].replaceAll("\"", "") === window.appSettings.identity.fontName
+                })
+            );
+        }
+    });
+
+    jQuery("#identity_twitchlogocolor").val(window.appSettings.identity.platformColor);
+    jQuery("#identity_twitchusername").val(window.appSettings.identity.channelName);
+    jQuery("#identity_twitchfulllink")[0].checked = window.appSettings.identity.fullChannelLink;
+    jQuery("#identity_titlescolor").val(window.appSettings.identity.colors.titles);
+    jQuery("#identity_dayoncolor").val(window.appSettings.identity.colors.dayOn);
+    jQuery("#identity_dayoffcolor").val(window.appSettings.identity.colors.dayOff);
+}
+
+export { initIdentityValues }
